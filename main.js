cbc61603ac68efca2365f6ab339597138acd53bd
mainsroft.www <93188370+elikUA@users.noreply.github.com>
2022-02-06 18:04:13 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+//------WHILE20--------//
+
+//alert('не вводите большие числа')
+// let a,b,c
+// let n =+prompt('n=')
+// let rez = 0
+// while(0<n) {
+//     a = n%10
+//     console.log(a)
+//     rez = rez*10+a
+//    n = (n-n%10)/10
+//    if (a==2) {
+//     console.log('true')
+// }else{
+//     console.log('false')
+// }
+// }
+
+//-----WHILE21--------//
+
+//alert('не вводите большие числа')
+// let a,b,c
+// let n =+prompt('n=')
+// let rez = 0
+// while(0<n) {
+//     a = n%10
+//     console.log(a)
+//     rez = rez*10+a
+//    n = (n-n%10)/10
+//    if (a%2 == 0){
+//     console.log('false')
+// }else{
+//     console.log('true')
+// }
+// }
+// console.log(rez)
+
+//-----WHILE22------//
+
+//alert('не вводите большие числа')
+// let n =+prompt('a=')
+// let b = 0
+// while (n>b) {
+//     if (n%b == 0) {
+//         console.log('false',n,'/',b)
+//     }else{
+//         console.log('true',n,'Non',b)
+//     }
+//     b++
+// }
